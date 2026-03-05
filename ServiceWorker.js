d0a3f0a9fbd189539c5015c39b59490b1c8f43b1
akvroeg <akvroeg@gmail.com>
2026-03-05 02:56:30 -0500

    Added lots of content

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Vroeginday-Engineering Portfolio-2026.03";
+const contentToCache = [
+    "Build/Build v2026.03.04.loader.js",
+    "Build/Build v2026.03.04.framework.js",
+    "Build/Build v2026.03.04.data",
+    "Build/Build v2026.03.04.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
